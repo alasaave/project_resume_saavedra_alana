7b71b4cbf5557500466ca353f32fe02b294ec298
Alana Saavedra <alasaave@gmail.com>
2018-04-12 13:16:51 -0500

    modify JS with (document).ready function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
-//Custom jQuery: highlight sections on mouseover
-$('section').mouseover(function () {
-    $(this).css({'background-color': 'dodgerblue', 'color': 'white'});
-});
-$('section').mouseout(function () {
-    $(this).css({'background-color': 'white', 'color': 'black'});
+$(document).ready(function () {
+    //Custom jQuery: highlight sections on mouseover
+    $('section').mouseover(function () {
+        $(this).css({'background-color': 'dodgerblue', 'color': 'white'});
+    });
+    $('section').mouseout(function () {
+        $(this).css({'background-color': 'white', 'color': 'black'});
+    });
+
+    //jQuery plugin: animate image on click
+    $('#btn').animateClick();
+
 });
